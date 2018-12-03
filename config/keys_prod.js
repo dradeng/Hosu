dc8c15cfa4d0b455013646a595692027557d9d7b
Draden Gaffney <dsg2dm@virginia.edu>
2018-12-03 14:20:38 -0500

    make it better

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  secretOrKey: process.env.SECRET_OR_KEY,
+  GoogleMapsApi: process.env.GOOGLE_MAPS_API,
+  IAmUserKey: process.env.I_AM_USER_KEY,
+  IAmUserSecret: process.env.I_AM_USER_SECRET,
+  BucketName: process.env.BUCKET_NAME,
 };
