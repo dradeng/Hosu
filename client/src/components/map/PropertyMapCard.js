bc013cfc1cf33869455a240a040df1f6a9768d12
dradeng <dsg2dm@virginia.edu>
2018-12-23 14:08:52 -0700

    map marker updated to be a class instead of variable and show price/reviews and not address

--- client/src/components/map/PropertyMapCard.js
+++ client/src/components/map/PropertyMapCard.js
@@ -1,42 +1,77 @@
-import React from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Item, Button } from "semantic-ui-react";
+import Rating from "react-rating";
+import { getProfile } from '../../actions/profileActions';
 
-const PropertyMapCard = (props) => {
+class PropertyMapCard extends Component {
+    componentDidMount() {
+        const { pro } = this.props
+        this.props.getProfile(pro.profile);
+    }
+    render() {
+        const { pro } = this.props
 
-    const { pro } = props
 
+        const { profile, loading } = this.props.profile;
+        let reviews = null;
 
+        if (profile === null || loading) {
+          //do nothing
+        } else {
+            var rating = profile.reviewSum / profile.numReviews;
+            reviews = <div>
+            <Rating
+                emptySymbol="far fa-star fa-2x"
+                fullSymbol="fa fa-star fa-2x"
+                readonly
+                initialRating={rating}
+                style={{fontSize: 8}}/> ({profile.numReviews})
+                
+            </div>
+        }
 
-    return (
-        <Item style={{maxWidth:120}}>
-            <Item.Image
-                src={pro.images[0]}
-                size="tiny"
-                className="img-responsive"
-                style={{width: "100%"}}
-            />
-            <Item.Content>
-                <Item.Header >
-                    <strong>
-                        {pro.title}
-                    </strong>
-                </Item.Header>
-                <Item.Meta>
-                    <span>
-                        <i>{pro.address.length
-                            ? pro.address
-                            : null}
-                        </i>
-                    </span>
-                    <br />
-                    <br />
-                    <span className="cinema">
-
-                    </span>
-                </Item.Meta>
-            </Item.Content>
-        </Item>
-    );
+        return (
+            <Item style={{maxWidth:120}}>
+                <Item.Image
+                    src={pro.images[0]}
+                    size="tiny"
+                    className="img-responsive"
+                    style={{width: "100%"}}
+                />
+                <Item.Content>
+                    <Item.Header >
+                        <strong>
+                            {pro.title}
+                        </strong>
+                    </Item.Header>
+                    <Item.Meta>
+                        <span>
+                            <i>
+                                ${pro.rent}
+                                {reviews}
+                            </i>
+                        </span>
+                        <br />
+                        <br />
+                        <span className="cinema"></span>
+                    </Item.Meta>
+                </Item.Content>
+            </Item>
+        );
+    }
 }
 
-export default PropertyMapCard;
+PropertyMapCard.propTypes = {
+  getProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfile })(
+  PropertyMapCard
+);
