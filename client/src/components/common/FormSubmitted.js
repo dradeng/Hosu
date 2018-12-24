b0ed94d2f4314a5ebb06690cced2aa1b100ef8a2
dradeng <dsg2dm@virginia.edu>
2018-12-24 06:26:57 -0700

    form submitted updated with automatic redirect

--- client/src/components/common/FormSubmitted.js
+++ client/src/components/common/FormSubmitted.js
@@ -1,12 +1,50 @@
-import React from 'react';
 
-const FormSubmitted = () => {
-  return (
-    <div>
-      <p>Form has been submitted successfully</p>
-    </div>
-  );
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profileActions';
+
+
+class FormSubmitted extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+    };
+  }
+  componentDidMount() {
+  	this.props.getCurrentProfile();
+
+	setTimeout(function() { //Start the timer
+	  this.setState({redirect: true}) //After 1 second, set render to true
+	}.bind(this), 5000)
+  }
+  render() {
+  	  const { user } = this.props.auth;
+
+  	  if(this.state.redirect) {
+  	  	return <Redirect to='/feed' />
+  	  }
+
+
+	  return (
+	    <div style={{textAlign:'center'}}>
+	      <h3>Thankyou {user.name}! Your form has been submitted successfully. This page will redirect to the feed in a few seconds</h3>
+	    </div>
+	  );
+    }
+}
+
+FormSubmitted.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
+const mapStateToProps = state => ({
+	profile: state.profile,
+	auth: state.auth
+});
 
-export default FormSubmitted;
+export default connect(mapStateToProps, { getCurrentProfile })(FormSubmitted);
