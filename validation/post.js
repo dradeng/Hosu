446bb6861038594c3a2fc89c5b4262107be4cd44
dradeng <dsg2dm@virginia.edu>
2018-12-26 00:14:05 -0500

    updating form validation

--- validation/post.js
+++ validation/post.js
@@ -6,6 +6,9 @@ module.exports = function validatePostInput(data) {
 
   data.text = !isEmpty(data.text) ? data.text : '';
   data.title = !isEmpty(data.title) ? data.title : '';
+  data.address = !isEmpty(data.address) ? data.address : '';
+  data.rent = !isEmpty(data.rent) ? data.rent : 0;
+  data.images = !isEmpty(data.images) ? data.images.length : 0;
 
   if (!Validator.isLength(data.text, { min: 1, max: 300 })) {
     errors.text = 'Post must be between 1 and 300 characters';
@@ -17,6 +20,18 @@ module.exports = function validatePostInput(data) {
 
   if (Validator.isEmpty(data.title)) {
     errors.title = 'Text field is required';
+  }
+
+  if (Validator.isEmpty(data.address)) {
+    errors.address = 'Address field is required';
+  }
+
+  if(data.rent < 1) {
+    errors.rent = 'Rent needs to above zero';
+  }
+
+  if(data.images == 0) {
+    errors.rent = 'Need atleast one image';
   }
 
   return {
