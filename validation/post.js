47670e31567fd998188b2ad0eddeb29aecea3fbf
dradeng <dsg2dm@virginia.edu>
2018-12-25 23:56:46 -0500

    validation for post updated

--- validation/post.js
+++ validation/post.js
@@ -5,6 +5,7 @@ module.exports = function validatePostInput(data) {
   let errors = {};
 
   data.text = !isEmpty(data.text) ? data.text : '';
+  data.title = !isEmpty(data.title) ? data.title : '';
 
   if (!Validator.isLength(data.text, { min: 1, max: 300 })) {
     errors.text = 'Post must be between 1 and 300 characters';
@@ -12,6 +13,10 @@ module.exports = function validatePostInput(data) {
 
   if (Validator.isEmpty(data.text)) {
     errors.text = 'Text field is required';
+  }
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = 'Text field is required';
   }
 
   return {
