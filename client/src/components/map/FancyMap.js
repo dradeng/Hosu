99f9f7c1496de01b2ed72e4f0193edc094934e86
dradeng <dsg2dm@virginia.edu>
2019-01-03 13:08:08 -0700

    refactoring google maps

---
+++ client/src/components/map/FancyMap.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { compose, withProps } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+import PropertyMarker from "./PropertyMarker"
+import MapContainer from "./MapContainer";
+import MarkerCircle from "./MarkerCircle";
+
+const Map = compose(
+        withProps({
+            googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBob7qS5XZFENWz5u8UCRFvLaOXxhh3geE",
+            loadingElement: <div style={{ height: `100%` }} />,
+            containerElement: <div style={{height: '100%',width: '100%'}} />,
+            mapElement: <div style={{ height: `100%` }} />,
+
+        }),
+        withScriptjs,
+        withGoogleMap
+        )((props) => {
+            var longitude = props.address.longitude;
+            var latitude = props.address.latitude;
+            var circle = props.circle;
+
+        var markers;
+
+        if(props.address.circle) {
+            markers = <MarkerCircle address={props.address}  />;
+
+        } else {
+            markers = props.propies.map( property => {
+                let marker = <PropertyMarker
+                    key={property._id}
+                    uid={property._id}
+                    circle={circle}
+                    closeMarkers={props.closeOtherMarkers}
+                    property={property}
+                    address={props.address}
+                    location={{lat: property.latitude, lng: property.longitude}}
+                    activeMarker={property._id === props.activeMarker ? true : false}/>
+                return marker;
+            });
+        }
+
+        return (
+        <GoogleMap
+            defaultZoom={10}
+            defaultCenter={{ lat: latitude, lng: longitude }}
+        >
+            {markers} 
+        </GoogleMap>
+        );
+    }
+);
+
+class FancyMap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          
+        };
+    }
+    componentDidMount() {
+
+    }
+    render() {
+        var address = this.props.address;
+        var circle = this.props.circle;
+        var properties = this.props.propies;
+        var toggleShowPage= this.props.toggleShowPage;
+        var activeMarker= this.props.activeMarker;
+        var closeOtherMarkers= this.props.closeOtherMarkers;
+        return (
+            <Map
+                address={address}
+                circle={circle}
+                propies={properties}
+                toggleShowPage={toggleShowPage}
+                activeMarker={activeMarker}
+                closeOtherMarkers={closeOtherMarkers}
+            />
+        );
+    }
+}
+
+export default FancyMap;
--- client/src/components/map/MapContainer.js
+++ client/src/components/map/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Map from "./Map";
+import FancyMap from "./FancyMap";
 
 class MapContainer extends Component {
     constructor(props) {
@@ -20,14 +20,9 @@ class MapContainer extends Component {
         const address = this.props.address;
         const circle = this.props.circle;
         return (
-            <Map
+            <FancyMap
                 address={address}
                 propies={properties}
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBob7qS5XZFENWz5u8UCRFvLaOXxhh3geE"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{height: '100%',width: '100%',
-                }} />}
-                mapElement={<div style={{ height: `100%` }} />}
                 toggleShowPage={this.props.toggleShowPage}
                 activeMarker={this.state.activeMarker}
                 closeOtherMarkers={this.closeOtherMarkers}
