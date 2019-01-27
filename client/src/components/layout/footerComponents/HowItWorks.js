f3d776bf240e353b74774c04e112198541d49d84
dradeng <dsg2dm@virginia.edu>
2019-01-27 01:56:20 -0500

    footer components done

--- client/src/components/layout/footerComponents/HowItWorks.js
+++ client/src/components/layout/footerComponents/HowItWorks.js
@@ -9,8 +9,37 @@ class HowItWorks extends Component {
 	 
 
 	  return (
-	    <div style={{textAlign:'center'}}>
-        HowItWorks
+	    <div>
+	    	<h1>How it works</h1>
+	    	<div style={{paddingLeft: 5}}>
+	        	Aveneu is disrupting the short-term lease market by offering a 
+	        	transformative marketplace. The marketplace is designed for 
+	        	flexible leases supporting two varities of leases:
+	        	
+	        	<br />
+	        	<br />
+
+	        	<div>
+					1. Flexible Stay: "Digital Nomad" 
+				</div>
+
+				
+
+				Perfect for newcomers who have moved on short notice. Rooms are leased for minimum stays. However, once you reserve a room, it is reserved for the upcoming weeks. You can therefore move in to a property staying week by week solving your temporary housing needs. 
+				<br />
+
+				<div>
+				<br />
+				2. Traditional Lease
+				</div>
+
+				Made for people who are in need of short-term housing for a given period. You can lease rooms for flexible periods set by property owners.
+
+				Whether you're a student, new grad, or digital nomad, Aveneu hopes to serve you. 
+				<br />
+				<br />
+				If you have any questions, email support@aveneu.co
+			</div>
 	    </div>
 	  );
     }
--- client/src/components/layout/footerComponents/TermsOfService.js
+++ client/src/components/layout/footerComponents/TermsOfService.js
@@ -9,9 +9,15 @@ class TermsOfService extends Component {
 	 
 
 	  return (
-	    <div style={{textAlign:'center'}}>
-        Terms of TermsOfService
-	    </div>
+	    <div> 
+		    <object 
+		    	type="text/html" 
+		    	data="https://app.termly.io/document/terms-of-use-for-online-marketplace/8056b8c8-8abe-4629-ac81-91b58816f80c" 
+		    	height="600px" 
+		    	width="1000px"
+		    	style={{overflow:'auto', display: 'table', margin: '0 auto'}}>
+		    </object>
+		 </div>
 	  );
     }
 }
